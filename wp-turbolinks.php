cd8ba9525777921110ce3d6ce5f33b220fb89e60
Keitaroh Kobayashi <keita@kkob.us>
2013-10-18 11:41:55 +0900

    `plugin_dir_url` comes with the trailing slash in it

--- wp-turbolinks.php
+++ wp-turbolinks.php
@@ -9,7 +9,7 @@ function wpt_enqueue_turbolinks() {
   if (WP_DEBUG)
     $js_file = 'wp-turbolinks.js';
 
-  wp_enqueue_script( 'wp-turbolinks', plugin_dir_url(__FILE__) . '/js/' . $js_file, array(), '1.0.0' );
+  wp_enqueue_script( 'wp-turbolinks', plugin_dir_url(__FILE__) . 'js/' . $js_file, array(), '1.0.0' );
 }
 
 // Make sure the turbolinks javascript is enqueued last.
@@ -22,7 +22,7 @@ function wpt_enqueue_jquery_turbolinks() {
   if (WP_DEBUG)
     $js_file = 'jquery.turbolinks.js';
 
-  wp_enqueue_script( 'jquery.turbolinks', plugin_dir_url(__FILE__) . '/js/' . $js_file, array( 'jquery' ), '2.1.0' );
+  wp_enqueue_script( 'jquery.turbolinks', plugin_dir_url(__FILE__) . 'js/' . $js_file, array( 'jquery' ), '2.1.0' );
 }
 
 add_action( 'wp_enqueue_scripts', 'wpt_enqueue_jquery_turbolinks', 1 );
